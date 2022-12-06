faef8c1a7749e302587501c472dec5079c7eed49
nicholasvp <nicholasvpinheiro@gmail.com>
2022-12-06 16:35:19 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,77 @@
+// DOM
+
+const buttonPlay = document.querySelector('.play')
+const buttonPause = document.querySelector('.pause')
+const buttonSet = document.querySelector('.set')
+const buttonStop = document.querySelector('.stop')
+const buttonSoundOn = document.querySelector('.soundOn')
+const buttonSoundOff = document.querySelector('.soundOff')
+let minutes
+const minutesDisplay = document.querySelector('.minutes')
+let seconds
+const secondsDisplay = document.querySelector('.seconds')
+
+//Função do timer
+function countdown(){
+    setTimeout (function(){
+        let seconds = Number(secondsDisplay.textContent)
+        let minutes = Number(minutesDisplay.textContent)
+
+        if (minutes === 0 && seconds === 0) {
+            
+            buttonStop.classList.add('hide')
+            buttonSet.classList.remove('hide')
+            buttonPause.classList.add('hide')
+            buttonPlay.classList.remove('hide')
+            return
+        }
+
+        if (seconds <= 0) {
+            seconds = 60
+
+            minutesDisplay.textContent = String(minutes - 1).padStart(2, "0")
+        }
+        
+        secondsDisplay.textContent = String(seconds -1).padStart(2, "0")
+
+        
+
+
+        countdown()
+    }, 1000)
+}
+
+//Evento de clique: mudar o ícone.
+buttonPlay.addEventListener('click', function(){
+    buttonPlay.classList.add('hide')
+    buttonPause.classList.remove('hide')
+    buttonSet.classList.add('hide')
+    buttonStop.classList.remove('hide')
+
+    countdown()
+})
+buttonPause.addEventListener('click', function(){
+    buttonPause.classList.add('hide')
+    buttonPlay.classList.remove('hide')
+})
+buttonStop.addEventListener('click', function(){
+    buttonStop.classList.add('hide')
+    buttonSet.classList.remove('hide')
+    buttonPause.classList.add('hide')
+    buttonPlay.classList.remove('hide')
+})
+buttonSoundOn.addEventListener('click', function(){
+    buttonSoundOn.classList.add('hide')
+    buttonSoundOff.classList.remove('hide')
+})
+buttonSoundOff.addEventListener('click', function(){
+    buttonSoundOff.classList.add('hide')
+    buttonSoundOn.classList.remove('hide')
+})
+
+//Evento do set: mudar os minutos e segundos
+buttonSet.addEventListener('click', function(){
+    minutes = prompt("Quantos minutos?")
+    minutesDisplay.textContent = String(minutes).padStart(2,'0')
+})
+
